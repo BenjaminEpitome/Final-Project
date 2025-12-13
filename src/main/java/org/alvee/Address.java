8a57f72806671673e2c775e69ef009ffd900478a
BenjaminEpitome <6326271@edu.vaniercollege.qc.ca>
2025-12-13 13:40:42 -0500

    Address: added toString() + equals() + hashCode()

--- src/main/java/org/alvee/Address.java
+++ src/main/java/org/alvee/Address.java
@@ -34,5 +34,55 @@ public class Address {
         }
         return true;
     }
+
     // args cons
+
+    public Address(Integer streetNo, String street, String city, Province province, String postalCode) {
+        if (isPostalCodeValid(postalCode)) {
+            this.streetNo = streetNo;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.postalCode = postalCode.toUpperCase();
+        } else {
+            this.streetNo = null;
+            this.street = null;
+            this.city = null;
+            this.province = null;
+            this.postalCode = null;
+        }
+        // toString
+
+    }
+    @Override
+    public String toString() {
+        return "Address{" +
+                "streetNo=" + streetNo +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", province=" + province +
+                ", postalCode='" + postalCode + '\'' +
+                '}';
+    }
+
+    // equals and hashcode
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Address)) return false;
+
+        Address other = (Address) obj;
+
+        return java.util.Objects.equals(streetNo, other.streetNo) &&
+                java.util.Objects.equals(street, other.street) &&
+                java.util.Objects.equals(city, other.city) &&
+                province == other.province &&
+                java.util.Objects.equals(postalCode, other.postalCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(streetNo, street, city, province, postalCode);
+    }
+
 }
