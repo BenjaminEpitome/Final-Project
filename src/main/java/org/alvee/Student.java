b491684a26a6f08d87e2c33c9e04cca27ce6455a
BenjaminEpitome <6326271@edu.vaniercollege.qc.ca>
2025-12-13 18:03:13 -0500

    Student class propreties and other things

--- src/main/java/org/alvee/Student.java
+++ src/main/java/org/alvee/Student.java
@@ -1,4 +1,33 @@
 package org.alvee;
 
+import java.util.ArrayList;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Student {
+    private String studentId;
+    private String studentName;
+    private Gender gender;
+    private Address address;
+    private Department department;
+    private ArrayList<Course> registeredCourses;
+
+    private static int nextId = 1;
+
+    // Constructor
+    public Student(String studentName, Gender gender, Address address, Department department) {
+        this.studentName = studentName;
+        this.gender = gender;
+        this.address = address;
+        this.department = department;
+
+        this.studentId = String.format("S%06d", nextId);
+        nextId++;
+
+        this.registeredCourses = new ArrayList<>();
+    }
+
 }
