3271421d1b939c7e88a3ab4a5baad8cb8c32d0f6
BenjaminEpitome <6326271@edu.vaniercollege.qc.ca>
2025-12-13 13:24:32 -0500

    Static helper in the class address

--- src/main/java/org/alvee/Address.java
+++ src/main/java/org/alvee/Address.java
@@ -1,4 +1,38 @@
 package org.alvee;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Address {
+
+    private Integer streetNo;
+    private String street;
+    private String city;
+    private Province province;
+    private String postalCode;
+
+    //Static helper
+    public static boolean isPostalCodeValid(String postalCode) {
+        if (postalCode == null || postalCode.length() != 6) {
+            return false;
+        }
+
+        for (int i = 0; i < 6; i++) {
+            char c = postalCode.charAt(i);
+
+            if (i % 2 == 0) { // letter positions
+                if (!Character.isLetter(c)) {
+                    return false;
+                }
+            } else { // digit positions
+                if (!Character.isDigit(c)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    // args cons
 }
