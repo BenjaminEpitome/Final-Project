a9b1ef85731b21e93e3573359e31d92d9b4c75e9
BenjaminEpitome <6326271@edu.vaniercollege.qc.ca>
2025-12-13 18:22:31 -0500

    Util class

--- src/main/java/org/alvee/Util.java
+++ src/main/java/org/alvee/Util.java
@@ -1,4 +1,34 @@
 package org.alvee;
 
 public class Util {
+
+    public static String toTitleCase(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+
+        String[] words = str.split(" ");
+        String result = "";
+
+        for (int i = 0; i < words.length; i++) {
+            String w = words[i];
+
+            if (w.length() > 0) {
+                String first = w.substring(0, 1).toUpperCase();
+                String rest = "";
+
+                if (w.length() > 1) {
+                    rest = w.substring(1).toLowerCase();
+                }
+
+                result += first + rest;
+            }
+
+            if (i < words.length - 1) {
+                result += " ";
+            }
+        }
+
+        return result;
+    }
 }
